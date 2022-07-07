1d15e2bb4f3addb6d08b8777395680bea5181b87
PROCESS-AGENT <lnrdoberg@gmail.com>
2022-07-07 20:21:42 -0300

    class

--- src/index.js
+++ src/index.js
@@ -6,13 +6,48 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const nome = 'Joao'
+//const nome = <h1>OPA, {nome} </h1>
 
 
+
+function Opa(props) {
+  return <h1>Opa, {props.name} {props.title} </h1>
+}
+/*
+function BoasVindas(props) {
+  return <Opa name= {props.name} title={props.title}>teste</Opa>
+}
+*/
+
+class BoasVindas extends React.Component {
+
+  render() {
+
+    return <div>
+      <h1>OPAs, {this.props.name}</h1>
+    </div>
+
+  }
+
+}
+
+
+root.render(
+  <div>
+    <Opa name='teste'></Opa>
+    <BoasVindas name='Maria'>teste 567</BoasVindas>
+    <BoasVindas></BoasVindas>
+    <BoasVindas></BoasVindas>
+    <BoasVindas></BoasVindas>
+  </div>
+
+);
+
+/*
 function tick() {
   const element = (
     <div>
-      <h1>Olá, Valdair!</h1>
+      <h1>Olá, Leonardo!</h1>
       <h2>Hora agora: {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
@@ -20,7 +55,7 @@ function tick() {
 }
 
 setInterval(tick, 1000);
-
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
