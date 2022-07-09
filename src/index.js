95d93db4cde15650a374a985f7018736d9a39526
PROCESS-AGENT <lnrdoberg@gmail.com>
2022-07-08 21:15:09 -0300

    lab04

--- src/index.js
+++ src/index.js
@@ -6,44 +6,52 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-class Relogio extends React.Component {
+class Lista extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { date: new Date() }
-
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
-  }
-
-  componentWillUnMount() {
-    clearInterval(this.timerID)
-  }
-
-  tick() {
-    this.setState({ date: new Date() })
-    console.log(this.state.date)
+    this.state = {
+      dados: [{
+        "cep": "20260-080",
+        "logradouro": "xpto",
+        "complemento": "até 392/393",
+        "bairro": "1",
+        "localidade": "Curitiba",
+        "uf": "RJ"
+      }, {
+        "cep": "20260-080",
+        "logradouro": "xpto 01",
+        "complemento": "até 392/393",
+        "bairro": "2",
+        "localidade": "Curitiba",
+        "uf": "RJ"
+      }, {
+        "cep": "20260-080",
+        "logradouro": "xpto 02",
+        "complemento": "até 392/393",
+        "bairro": "3",
+        "localidade": "Curitiba",
+        "uf": "RJ"
+      }]
+    }
   }
 
   render() {
     return (
       <div>
-        <p>Opa!! do componente do Relógio!</p>
-        <h2>HOra agora: {this.state.date.toLocaleTimeString()} </h2>
+        <h2>Opa</h2> 
+        <ul>
+          {this.state.dados.map((endereco) => <li key={endereco.bairro}>{endereco.logradouro} {endereco.cep}</li>)}
+        </ul>
       </div>
-    );
+    )
   }
 }
 
 
 root.render(
-  <div>
-    <Relogio>
+  <Lista />
+)
 
-    </Relogio>
-  </div>
-);
 
 
 
