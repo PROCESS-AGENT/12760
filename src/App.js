ed3ac03a20c1cbe81898d8fbd8d93355f822a6ca
PROCESS-AGENT <lnrdoberg@gmail.com>
2022-07-13 18:36:44 -0300

    Cpmponentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
+import ListProductComponent from './components/ListProductComponent'
+import CreateProductComponent from './components/CreateProductComponent'
 
 function App() {
-  return (
-    <div className="App">
-     App
-    </div>
+  return (<div>
+    <Router>
+      <HeaderComponent />
+      <div className="container">
+        <ListProductComponent />
+        {/* <CreateProductComponent /> */}
+      </div>
+
+      <FooterComponent />
+    </Router>
+
+  </div>
   );
 }
 
-export default App;  
+export default App;
---
+++ src/components/CreateProductComponent.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class CreateProductComponent extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (<div>
+            <h1>Criar Produto</h1>
+        </div>)
+    }
+}
+
+export default CreateProductComponent;
